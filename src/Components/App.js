29a585c41635d469dc252a6f7e72676fb4015711
Yerrumsetty Bharath <yerrumsett.bharath@gmail.com>
2021-11-24 21:21:35 +0530

    testing with fake apis

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,31 +6,54 @@ import { useState ,useEffect} from 'react';
 import {uuid} from 'uuidv4'; 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ContactDetail from './ContactDetail';
+import api from "../api/contacts.js";
 
 
 const App = ()=> {
 
   const Local_Storage_key = "contacts";
   const [contacts, setContacts] = useState([])
-  const addContactHandler = (contact) =>{
-    setContacts([...contacts,{id:uuid(),...contact}])
+  const addContactHandler = async (contact) =>{
+
+    const request = {
+      id:uuid(),
+      ...contact
+    }
+
+    const response = await api.post("/contacts",request)
+    setContacts([...contacts, response.data])
   }
 
-  const removeContactHandler =(id) =>{
+  const retrieveContacts = async () => {
+    const response = await api.get("/contacts");
+    return response.data;
+  }
+
+  const removeContactHandler = async (id) =>{
+    await api.delete(`contacts/${id}`);
     const newList = contacts.filter((item)=>item.id!==id);
     setContacts(newList);
   }
 
   useEffect(()=>{
-    const retrieveContacts = JSON.parse(localStorage.getItem(Local_Storage_key));
-    if(retrieveContacts)
-    setContacts(retrieveContacts);
+    // const retrieveContacts = JSON.parse(localStorage.getItem(Local_Storage_key));
+    // if(retrieveContacts)
+    // setContacts(retrieveContacts);
+    const getAllContacts = async () =>{
+      const allContacts = await retrieveContacts();
+      if(allContacts)
+      {
+        setContacts(allContacts);
+      }
+    }
+
+    getAllContacts();
   },[])
 
   
   //store array in the local storage so that data doesnt get erased after resfreshing
   useEffect(()=>{
-    localStorage.setItem(Local_Storage_key,JSON.stringify(contacts));
+    // localStorage.setItem(Local_Storage_key,JSON.stringify(contacts));
   },[contacts])
 
  
